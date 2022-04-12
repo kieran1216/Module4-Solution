ae11ea25103f5628851da340796ad70262d5f187
kieran1216 <ksamir@ad.unc.edu>
2022-04-12 11:42:19 -0400

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -17,7 +17,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak=function(name) {
-  console.log(speakword + " " + name);
+  console.log(speakWord + " " + name);
 }
 
 
